93d862b64f0bb7aaa1bd9289d3235337a5e6fb2d
Jaxson Montanaro <jaxsonmontanaro@gmail.com>
2025-04-06 17:58:47 -0700

    updated pseudocode

--- Withholdings.java
+++ Withholdings.java
@@ -5,8 +5,28 @@
  * If income less than $500: return income times .1 as tax rate is 10%
  * Income is greater than/equal to $500 and less than $1500: return income times .15 tax rate is 15%
  * Income is greater than/equal to $1500 and less than $2500: return income times .2 tax rate is 20%
- * Income is greater than/equal to $2500: return income times .3 tax rate is 30% */
-
+ * Income is greater than/equal to $2500: return income times .3 tax rate is 30% 
+ * 
+ * 
+ * Function Query
+ * Return (What is your weekly income?)
+ * int income = UserInput
+ * 
+ * Function TaxCalculator {
+ *  IF income < 500 THEN
+ * return (income times .1);
+ * ELSE IF income income >= 500 && income < 1500 THEN
+ * return (income times .15);
+ * ELSE IF income income >= 1500 && income < 2500 THEN
+ * return (income times .20);
+ * ELSE IF income income >= 2500 THEN
+ * return (income times .30);
+ * ELSE
+ * return (please enter a valid number)
+ * }
+ * 
+ * */
+ 
  import java.util.Scanner;
  //imports scanner and allows user to answer prompt
 
@@ -26,13 +46,10 @@
         System.out.println("your tax withholding is " + (income*.15) + "dollars");
     }
     else if (income >= 1500 && income < 2500) {
-        System.out.println("your tax withholding is " + (income*.15) + "dollars");
-    }
-    else if (income >= 1500 && income < 2500) {
-        System.out.println("your tax withholding is " + (income*.15) + "dollars");
+        System.out.println("your tax withholding is " + (income*.2) + "dollars");
     }
     else if (income >= 2500) {
-        System.out.println("your tax withholding is " + (income*.15) + "dollars");
+        System.out.println("your tax withholding is " + (income*.3) + "dollars");
     }
     else {
         System.out.println("please enter a valid number");
